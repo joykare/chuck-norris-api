e4049c2b29bfc548428aa6937c0d4fc97db66b50
joykare <warugujoy@gmail.com>
2018-06-22 14:29:06 +0300

    (): comment out redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import thunk from "redux-thunk";
 const configureStore = () => {
   return createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
   );
 };
